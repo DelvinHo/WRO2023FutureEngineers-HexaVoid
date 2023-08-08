df10a0cb46d34b5c9f41cc39098b1f8532af5b33
Felix Isaac <me@felixisaac.dev>
2023-08-08 11:31:41 +0800

    Update ultrasonic_sensor.py

--- 4. Code/ultrasonic_sensor.py
+++ 4. Code/ultrasonic_sensor.py
@@ -5,17 +5,17 @@ import time
 class UltrasonicSensor:
     def __init__(self, trigger: int, echo: int) -> None:
         """Initialise a ultrasonic sensor object.
-        
+
         The ultrasonic sensor US-015 used for the measurement of distances
         between the ultrasonic sensor and the object in front of the sensor.
-        
+
         https://sg.cytron.io/p-high-precision-ultrasonic-range-finder-us-015
-        
+
         Args:
             trigger (int): The GPIO pin number connected to the Trigger output of the ultrasonic sensor
             echo (int): The GPIO pin number connected to the Echo input of the ultrasonic sensor
         """
-        
+
         self.trigger = trigger
         self.echo = echo
 
@@ -29,13 +29,15 @@ class UltrasonicSensor:
             float: The calculated distance in centimetres.
         """
 
-        # Set trigger pin to LOW for stabilisation
+        # Set trigger pin to LOW for stabilisation. To lower the duration after testing
         GPIO.output(self.trigger, GPIO.LOW)
-        time.sleep(0.2)  # Stabilise the sensor
+        time.sleep(0.2)
 
-        GPIO.output(self.trigger, GPIO.HIGH)
+        # Trigger for 10 microseconds, should send 8 ultrasonic bursts at 40KHz
         # Try 0.01 if this doesn't work, most OS support 10 ms at least
+        GPIO.output(self.trigger, GPIO.HIGH)
         time.sleep(0.00001)
+
         GPIO.output(self.trigger, GPIO.LOW)
 
         start_time = time.time()
