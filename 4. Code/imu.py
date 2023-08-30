f5b0868c14ec4f33c22b0b6d837b1dddd0f3a91e
FelixIsaac <limfelix16@gmail.com>
2023-08-30 22:02:58 +0800

    feat(imu): kalman filter for reduction in noise - not tested 'cause library not installed properly

--- 4. Code/imu.py
+++ 4. Code/imu.py
@@ -1,5 +1,7 @@
 from typing import TypedDict, List
+
 from smbus2 import SMBus
+from filterpy.kalman import KalmanFilter
 
 """
 If you encounter the following error, FileNotFoundError: [Errno 2] No such file or directory: '/dev/i2c-1'
@@ -30,11 +32,24 @@ class IMU:
         self.address = address
         self.bus = 1
 
+        # Initialize Kalman Filter
+        self.kf = KalmanFilter(dim_x=6, dim_z=6)
+
+        # Set initial state and covariance
+        self.kf.x = np.zeros(6)   # Initial state: [accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z]
+        self.kf.P = np.eye(6)     # Initial state covariance
+
+        # Set state transition matrix F, observation matrix H, process noise covariance Q, and measurement noise covariance R
+        self.kf.F = np.eye(6)     # Identity matrix for state transition (simple integration)
+        self.kf.H = np.eye(6)     # Identity matrix for observation (direct measurements)
+        self.kf.Q *= 0.01         # Adjust process noise covariance based on your system
+        self.kf.R *= 0.1          # Adjust measurement noise covariance based on your system
+
         with SMBus(self.bus) as bus:
             # Wake up the MPU6050
             bus.write_byte_data(self.address, PWR_MGMT_1, 0)
 
-    def read_word(self, reg: hex):
+    def read_word(self, reg: hex) -> int:
         with SMBus(self.bus) as bus:
             high = bus.read_byte_data(self.address, reg)
             low = bus.read_byte_data(self.address, reg + 1)
@@ -64,13 +79,21 @@ class IMU:
             self.read_word(reg + GYRO_ZOUT_H) * gyro_conversion
         )
 
+        # Use Kalman Filter for prediction and update
+        filtered_data = self.kf.predict_update([accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z])
+
+        # Return the filtered data
+        filtered_accel = filtered_data[:3]
+        filtered_gyro = filtered_data[3:]
+        
         """
         acceleration is in g but then converted to m/s^2
         gyroscope is angular velocity (degrees per second)
         """
+
         data: AccelGyroData = {
-            'accel': [accel_x, accel_y, accel_z],
-            'gyro': [gyro_x, gyro_y, gyro_z]
+            'accel': filtered_accel,
+            'gyro': filtered_gyro
         }
 
         return data
