1df0de09d64025d88742c2dc309e124488b41522
FelixIsaac <limfelix16@gmail.com>
2023-08-31 12:22:18 +0800

    fix(kalman filter): no update_predict method

--- 4. Code/imu.py
+++ 4. Code/imu.py
@@ -1,6 +1,7 @@
 from typing import TypedDict, List
 
 from smbus2 import SMBus
+from numpy import eye, zeros
 from filterpy.kalman import KalmanFilter
 
 """
@@ -26,7 +27,7 @@ class AccelGyroData(TypedDict):
     gyro: List[float]
 
 class IMU:
-    def __init__(self, SCL_pin: int, SDA_pin: int, address: hex) -> None:
+    def __init__(self, SCL_pin: int, SDA_pin: int, address: hex = 0x68) -> None:
         self.SCL_pin = SCL_pin
         self.SDA_pin = SDA_pin
         self.address = address
@@ -36,14 +37,14 @@ class IMU:
         self.kf = KalmanFilter(dim_x=6, dim_z=6)
 
         # Set initial state and covariance
-        self.kf.x = np.zeros(6)   # Initial state: [accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z]
-        self.kf.P = np.eye(6)     # Initial state covariance
+        self.kf.x = zeros(6)   # Initial state: [accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z]
+        self.kf.P = eye(6)     # Initial state covariance
 
         # Set state transition matrix F, observation matrix H, process noise covariance Q, and measurement noise covariance R
-        self.kf.F = np.eye(6)     # Identity matrix for state transition (simple integration)
-        self.kf.H = np.eye(6)     # Identity matrix for observation (direct measurements)
-        self.kf.Q *= 0.01         # Adjust process noise covariance based on your system
-        self.kf.R *= 0.1          # Adjust measurement noise covariance based on your system
+        self.kf.F = eye(6)     # Identity matrix for state transition (simple integration)
+        self.kf.H = eye(6)     # Identity matrix for observation (direct measurements)
+        self.kf.Q *= 0.01      # Adjust process noise covariance based on your system
+        self.kf.R *= 0.1       # Adjust measurement noise covariance based on your system
 
         with SMBus(self.bus) as bus:
             # Wake up the MPU6050
@@ -80,7 +81,9 @@ class IMU:
         )
 
         # Use Kalman Filter for prediction and update
-        filtered_data = self.kf.predict_update([accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z])
+        self.kf.predict()
+        self.kf.update([accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z])
+        filtered_data = self.kf.x
 
         # Return the filtered data
         filtered_accel = filtered_data[:3]
